e8e3342e4b80919cf84b5514c336408beaee7360
Muhammed Sahad P <zanusahad@gmail.com>
2024-09-04 11:49:42 +0530

    give link for logo to navigate to dashboard

--- src/Admin/AdminComponents/Navbar.jsx
+++ src/Admin/AdminComponents/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import { GiRunningShoe } from "react-icons/gi";
 import { RiAdminFill } from "react-icons/ri";
 
@@ -14,12 +15,12 @@ const Navbar = () => {
   return (
     <nav className="bg-white shadow-md p-4">
       <div className="container mx-auto flex items-center justify-between">
-        <div className="flex items-center">
+        <Link to="/dashboard" className="flex items-center">
           <GiRunningShoe className="text-red-700 text-3xl" />
           <span className="text-2xl font-serif font-bold text-gray-800 ml-2">
             FootFusion
           </span>
-        </div>
+        </Link>
      
         <button
           onClick={toggleMenu}
